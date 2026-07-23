1b3bac0b85e2f2adb87cfeed6223853767ceb3cb
Ian Johnson <IJohnso8@Depaul.edu>
2026-07-22 22:15:33 -0400

    Updated README with Design Pattern info and other Sprint 3 related info.  Added comments to the current Main.java file.

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,14 +6,18 @@ import main.restaurant.menu.Combo;
 import main.restaurant.order.Order;
 
 public class Main {
-    public static void main(String[] args) {
+    static void main() {
+
+        // creates an order number 1
         Order order = new Order(1);
 
         IComboCreator lunchCreator = new LunchComboCreator();
         Combo lunch = lunchCreator.createCombo();
 
+        // adds a lunch combo to order number 1
         order.addItem(lunch);
 
+        // prints the total of a lunch combo that is added to the User's order
         System.out.println("Total: " + order.getTotalPrice());
     }
 }
